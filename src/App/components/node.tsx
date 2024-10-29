efba56926de561090302ed67171dad254fd69ce4
Harsh Vats <vatsvatsharsh2000@gmail.com>
2024-10-29 19:59:16 +0530

    feat: fix node scrollbar

--- src/App/components/node.tsx
+++ src/App/components/node.tsx
@@ -105,6 +105,7 @@ export function TextUpdaterNode({ data, id }: { data: NodeData; id: string }) {
         ) : (
           <div
             onClick={onSingleClick}
+            className="no-scrollbar"
             style={{
               overflow: "scroll",
               border: "none",
